23da6f9b444e8a33c794d9d5ae8c024be76d690f
Olga Botvinnik <olga.botvinnik@gmail.com>
2020-03-27 10:28:02 -0700

    Merge bin folder"

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -3,31 +3,6 @@ from __future__ import print_function
 from collections import OrderedDict
 import re
 
-<<<<<<< HEAD
-regexes = {
-    'nf-core/nf-kmer-similarity': ['v_pipeline.txt', r"(\S+)"],
-    'Nextflow': ['v_nextflow.txt', r"(\S+)"],
-    'Sourmash': ['v_sourmash.txt', r"sourmash version (\S+)"],
-}
-results = OrderedDict()
-results['nf-core/nf-kmer-similarity'] = '<span style="color:#999999;\">N/A</span>'
-results['Nextflow'] = '<span style="color:#999999;\">N/A</span>'
-results['Sourmash'] = '<span style="color:#999999;\">N/A</span>'
-
-# Search each file using its regex
-for k, v in regexes.items():
-    with open(v[0]) as x:
-        versions = x.read()
-        match = re.search(v[1], versions)
-        if match:
-            results[k] = "v{}".format(match.group(1))
-
-# Dump to YAML
-print ('''
-id: 'nf-core/nf-kmer-similarity-software-versions'
-section_name: 'nf-core/nf-kmer-similarity Software Versions'
-section_href: 'https://github.com/czbiohub/nf-kmer-similarity'
-=======
 # TODO nf-core: Add additional regexes for new tools in process get_software_versions
 regexes = {
     'nf-core/kmermaid': ['v_pipeline.txt', r"(\S+)"],
@@ -62,25 +37,16 @@ print ('''
 id: 'software_versions'
 section_name: 'nf-core/kmermaid Software Versions'
 section_href: 'https://github.com/nf-core/kmermaid'
->>>>>>> upstream/TEMPLATE
 plot_type: 'html'
 description: 'are collected at run time from the software output.'
 data: |
     <dl class="dl-horizontal">
 ''')
 for k,v in results.items():
-<<<<<<< HEAD
-    print("        <dt>{}</dt><dd>{}</dd>".format(k,v))
-print ("    </dl>")
-
-# Write out regexes as csv file:
-with open('software_versions.txt', 'w') as f:
-=======
     print("        <dt>{}</dt><dd><samp>{}</samp></dd>".format(k,v))
 print ("    </dl>")
 
 # Write out regexes as csv file:
 with open('software_versions.csv', 'w') as f:
->>>>>>> upstream/TEMPLATE
     for k,v in results.items():
         f.write("{}\t{}\n".format(k,v))
